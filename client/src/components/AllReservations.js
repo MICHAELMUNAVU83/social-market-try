3de2fada92e541a94043b503eab8d57879afca99
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-11-29 17:41:14 +0300

    Added all reservations component

--- client/src/components/AllReservations.js
+++ client/src/components/AllReservations.js
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
 function AllReservations() {
+  const [reservations, setReservations] = useState([]);
+  useEffect(() => {
+    fetch("/api/v1/reservations")
+      .then((res) => res.json())
+      .then((data) => {
+        setReservations(data);
+        console.log(data);
+      });
+  }, []);
+  const allReservations = reservations.map((reservation) => (
+    <div>
+      This is a reservation for {reservation.name} at {reservation.event.name} by {reservation.user.username}
+    </div>
+  ));
+
   return (
-    <div>AllReservations</div>
-  )
+    <div>
+      AllReservations
+      {reservations.length > 0 ? allReservations : "No reservations"}
+    </div>
+  );
 }
 
-export default AllReservations
+export default AllReservations;
