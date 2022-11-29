52b8a7d31149eea41f4a30d5f78c704b450e25ae
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-11-29 09:38:58 +0300

    Added event controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_28_082448) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_063719) do
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "venue"
+    t.date "date"
+    t.integer "average_age"
+    t.string "highest_gender_represented"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
