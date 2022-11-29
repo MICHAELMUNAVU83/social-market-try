869f172213e2d4d31ed9fe691d1259d92ef473e5
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-11-29 13:45:16 +0300

    Added routes and actions

--- client/src/components/AddVendorCategory.js
+++ client/src/components/AddVendorCategory.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 function AddVendorCategory() {
-  return <div>AddVendorCategory</div>;
+  const params = useParams();
+  const [category, setCategory] = useState("");
+  const [vendorPassesPerSlot, setVendorPassesPerSlot] = useState("");
+  const [ammenities, setAmmenities] = useState("");
+  const [cost, setCost] = useState("");
+
+  return (
+    <div>
+      AddVendorCategory
+      <form onSubmit={addVendorCategory}>
+        <label>Category</label>
+        <input
+          type="text"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        />
+        <label>Vendor Passes Per Slot</label>
+        <input
+          type="text"
+          value={vendorPassesPerSlot}
+          onChange={(e) => setVendorPassesPerSlot(e.target.value)}
+        />
+        <label>Ammenities</label>
+        <input
+          type="text"
+          value={ammenities}
+          onChange={(e) => setAmmenities(e.target.value)}
+        />
+        <label>Cost</label>
+        <input
+          type="text"
+          value={cost}
+          onChange={(e) => setCost(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default AddVendorCategory;
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:create]
       resources :events, only: [:index, :show, :create, :update, :destroy]
-      resources :vendor_categories, only: [:index]
+      resources :vendor_categories, only: [:index, :show, :create, :update, :destroy]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
     end
