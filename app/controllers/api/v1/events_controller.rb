a8f61501f795660ef53b0addc0d2b68001d7f1f8
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-11-29 09:47:10 +0300

    Added create action

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -4,4 +4,18 @@ class Api::V1::EventsController < ApplicationController
         events = Event.all
         render json: events
     end
+
+    def create
+        event = Event.create(event_params)
+        if event.valid?
+            render json: event
+        else
+            render json: {error: "Error creating event"}
+        end
+    end
+
+    private
+    def event_params
+        params.require(:event).permit(:name, :venue, :date, :average_age, :highest_gender_represented, :artists, :event_poster)
+    end
 end
