f4a5ed120eaaff927a7876d71ef884d2a4dbdc66
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-12-02 19:49:39 +0300

    Added splide

--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
 
 function Events() {
   const [events, setEvents] = useState([]);
@@ -12,18 +14,37 @@ function Events() {
   }, []);
   console.log(events);
   const AllEvents = events.map((event) => (
-    <div>
-      <p>{event.name}</p>
-      <img src={event.event_poster} alt="event poster" />
-      <Link to={`/events/${event.id}`}>Show</Link>
-    </div>
+    <SplideSlide>
+      <div>
+        <img src={event.event_poster} alt="event poster" />
+        <p>{event.name}</p>
+        Date of Event: {event.date}
+        <br />
+        <Link to={`/events/${event.id}`}>Show</Link>
+      </div>
+    </SplideSlide>
   ));
 
   return (
     <div>
       <h3 className="text-center">Events</h3>
-      
-      {AllEvents}
+      <Splide
+        options={{
+          perPage: 3,
+          arrows: false,
+          autoplay: false,
+          speed: 8000,
+          pagination: false,
+          type: "loop",
+          interval: 6000,
+          rewindByDrag: true,
+          drag: "free",
+          gap: "2rem",
+        }}
+        aria-label="My Favorite Images"
+      >
+        {AllEvents}
+      </Splide>
     </div>
   );
 }
