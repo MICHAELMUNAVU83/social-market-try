fe1669ecbd4fa6114d54ae3b39d1c43f948a056d
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-12-05 13:55:01 +0300

    Added client reservations

--- client/src/components/AllReservations.js
+++ client/src/components/AllReservations.js
@@ -10,8 +10,51 @@ function AllReservations() {
       });
   }, []);
   const allReservations = reservations.map((reservation) => (
-    <div>
-      This is a reservation for {reservation.name} at {reservation.event.name} by {reservation.user.username}
+    <div className="card my-4">
+      <div className="card-header text-center">
+        Here are the details of your vending reservation for{" "}
+        User {reservation.name} with phone number {reservation.phone_number}
+      </div>
+      <div className="d-flex justify-content-center  container">
+        <div className="card-body">
+          <h5 className="card-title">
+            <span>Event: </span>
+            {reservation.event.name}
+          </h5>
+          <p className="card-text">
+            <span>Event Date: </span>
+            {reservation.event.date}
+          </p>
+          <p className="card-text">
+            <span>Event Venue : </span>
+            {reservation.event.venue}
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+        <div className="card-body">
+          <h5 className="card-title">
+            <span>Vending Category: </span>
+            {reservation.vendor_category.category}
+          </h5>
+          <p className="card-text">
+            <span>Vendor Passes: </span>
+            {reservation.vendor_category.vendor_passes_per_slot}
+          </p>
+          <p className="card-text">
+            <span>Ammenities: </span>
+            {reservation.vendor_category.amenities_provided}
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+      <div className="card-footer text-center text-muted">
+        You will be contacted to pay the reservation fee of{" "}
+        {reservation.vendor_category.cost_per_slot}
+      </div>
     </div>
   ));
 
