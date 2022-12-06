7166bf820f03323b4ae0b9f2ac57d42f9247e56c
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-12-06 06:21:26 +0300

    added icons to signup

--- client/src/components/AllReservations.js
+++ client/src/components/AllReservations.js
@@ -12,7 +12,7 @@ function AllReservations() {
   const allReservations = reservations.map((reservation) => (
     <div className="card my-4">
       <div className="card-header text-center">
-        Here are the details of your vending reservation for User{" "}
+        Here are the details of your vending reservation for {" "}
         {reservation.name} with phone number {reservation.phone_number}
       </div>
       <div className="d-flex justify-content-center  container">
@@ -29,9 +29,10 @@ function AllReservations() {
             <span>Event Venue : </span>
             {reservation.event.venue}
           </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
+          <p className="card-text">
+            <span>Time : </span>
+            {reservation.event.time_event_starts}
+          </p>
         </div>
         <div className="card-body">
           <h5 className="card-title">
@@ -39,20 +40,21 @@ function AllReservations() {
             {reservation.vendor_category.category}
           </h5>
           <p className="card-text">
-            <span>Vendor Passes: </span>
+            <span>Vendor Passes to Provide</span>
             {reservation.vendor_category.vendor_passes_per_slot}
+          </p>
+          <p className="card-text">
+            <span>Money to Collect:</span>
+            {reservation.vendor_category.cost_per_slot}
           </p>
           <p className="card-text">
             <span>Ammenities: </span>
             {reservation.vendor_category.amenities_provided}
           </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
         </div>
       </div>
       <div className="card-footer text-center text-muted">
-        Contact {reservation.name} the reservation fee of{" "}
+        Contact {reservation.name} to pay  the reservation fee of{" "}
         {reservation.vendor_category.cost_per_slot}
       </div>
     </div>
